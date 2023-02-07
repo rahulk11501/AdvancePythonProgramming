31029c91aab8498006867b1f7374dde711cf45f7
Rahul Kumar <rahul.kumar@littlebirdie.com.au>
2023-02-07 21:30:10 +0530

    Adding print list def

--- LinkList/sll.py
+++ LinkList/sll.py
@@ -1,3 +1,4 @@
+# Single Link List Implementation
 class Node:
   def __init__(self, data):
     self.data = data
@@ -8,6 +9,12 @@ class SLL:
   def __init__(self):
     self.head = None
 
+  def printlist(self):
+    output = self.head
+    while output is not None:
+      print(output.data)
+      output = output.next
+
 
 list1 = SLL()
 # print(Node("Mon").next)
@@ -17,7 +24,13 @@ node3 = Node("Wed")
 list1.head = node1
 
 node1.next = node2
+
+# Following all print same value
 print(node2)
 print(list1.head.next)
 print(node1.next)
+
+
 node2.next = node3
+
+list1.printlist()
