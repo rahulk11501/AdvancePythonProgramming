4ca2b5774f99e16ce3bae27e8f17322f4c1b932c
Rahul Kumar <rahulkumar11501@gmail.com>
2023-08-10 05:18:50 +0000

    Adding del functionality DLL

--- link_list/DLL/deletion.py
+++ link_list/DLL/deletion.py
@@ -33,6 +33,25 @@ class DLL():
             else:
                 # print("ptr.data", ptr.data)
                 ptr = ptr.next
+    
+    def delete_at_start(self):
+        """ Function deletes node at start of DLL """
+        ptr = self.head.next
+        ptr.prev = None
+        self.head = ptr
+    
+    def delete_at_middle(self, data):
+        """ Function deletes any node of DLL """
+        ptr = self.head
+        while ptr.next is not None:
+            if ptr.data == data:
+                temp = ptr
+                temp.prev.next = temp.next
+                temp.next.prev = temp.prev
+                ptr = ptr.next
+                del(temp)
+            else:
+                ptr = ptr.next
 
     def append_at_end(self, data):
         """ Function appends a node at end """
@@ -59,9 +78,14 @@ def main():
     d_list.append_at_end("Sun")
     d_list.append_at_end("Mon")
     d_list.append_at_end("Tue")
+    d_list.append_at_end("Wed")
+    d_list.append_at_end("Thu")
+    d_list.append_at_end("Fri")
+    d_list.append_at_end("Sat")
     d_list.print_list()
     print("***** Delete at end ********")
-    d_list.delete_at_end()
+    d_list.delete_at_start()
+    d_list.delete_at_middle("Wed")
     d_list.print_list()
 main()
     
